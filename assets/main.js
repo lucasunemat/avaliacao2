b2d3e3bafa479c2e1fd7ca84cd0337ce3315c869
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-10-01 01:01:36 -0400

    second commit

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+const constHamburguer = document.querySelector(".hamburguer");
+const constNav = document.querySelector(".cabecalho-navegacao");
+const constAudio = document.querySelector(".audio");
+constHamburguer.addEventListener("click", () =>
+    constNav.classList.toggle("active"));
