7a9c75703a20436d7593b1f549f1a71bfb1e502b
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-10-01 18:42:30 -0400

    adição do link spotify

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,5 @@ const constHamburguer = document.querySelector(".hamburguer");
 const constNav = document.querySelector(".cabecalho-navegacao");
 const constAudio = document.querySelector(".audio");
 constHamburguer.addEventListener("click", () =>
-    constNav.classList.toggle("active"));
+    constNav.classList.toggle("active")
+);
