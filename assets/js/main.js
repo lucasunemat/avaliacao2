189cb3d3f44a0eece6b01bb1a6bc99703bdc1b09
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-11-10 12:42:30 -0400

    inicio dos ajustes para ajax no site

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const constHamburguer = document.querySelector(".hamburguer");
+const constNav = document.querySelector(".cabecalho-navegacao");
+constHamburguer.addEventListener("click", () =>
+    constNav.classList.toggle("active"),
+);
+
--- assets/main.js
+++
@@ -1,5 +0,0 @@
-const constHamburguer = document.querySelector(".hamburguer");
-const constNav = document.querySelector(".cabecalho-navegacao");
-constHamburguer.addEventListener("click", () =>
-    constNav.classList.toggle("active"),
-);
