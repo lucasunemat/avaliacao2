acc45e77a830a81d45208b9a346a70ba5609fa4a
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-11-10 13:35:49 -0400

    tentando evitar o home duplicado

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,1 +1,31 @@
 'use strict';
+
+document.addEventListener('click', e => {
+    const elemento = e.target;
+    const tag = elemento.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(elemento);
+    }
+});
+
+async function carregaPagina(elemento) {
+    try {
+        const href = elemento.getAttribute('href');
+        const response = await fetch(href); //fetch é promise, vai retornar response se der certo
+
+        if (response.status !== 200) throw new Error('Erroooo');
+
+        const html = await response.text(); //text() é outra promise, vai retornar um html se der certo
+        carregaConteudo(html); //carrega dados retornados
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function carregaConteudo(response) {
+    const main = document.getElementsByTagName('main')[0];
+    main.innerHTML = response;
+}
